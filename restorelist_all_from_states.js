c84ae72b2dddf2897a0baa535d8f2e2c39be38ad
Jiyong Youn <01@0101010101.com>
2022-09-07 23:28:19 +0900

    Optimization

--- restorelist_all_from_states.js
+++ restorelist_all_from_states.js
@@ -1,6 +1,7 @@
 //--max_old_space_size=4096
 
 const mysql = require('mysql');
+const mariadb = require('mariadb');
 const fs = require('fs');
 const { ArgumentParser } = require('argparse');
 
@@ -57,56 +58,77 @@ let args = parser.parse_args();
 block_start = args.start;
 block_end = args.end;
 epoch_inactivate_every = args.inactivate_every;
-epoch_inactivate_older_than = args.epoch_inactivate_older_than;
+epoch_inactivate_older_than = args.inactivate_older_than;
 output_restore = args.output_filename;
 log_period = args.log_every;
 
 async function run(from, to) {
-  conn = await mysql.createConnection({host: db_host, user: db_user, password: db_pass, database: db_name});
-  await conn.connect();
-  for (let i = from; i <= to; i+=1) {
+  conn = await mariadb.createConnection({host: db_host, user: db_user, password: db_pass, database: db_name});
+  //await conn.connect();
+  let batch_size = 100;
+  for (let i = from; i <= to; i+=batch_size) {
     //read, write
-    let query = await mysql_query(conn, "SELECT `address`, `blocknumber`, `type` FROM `states` LEFT JOIN `addresses` ON `states`.`address_id`=`addresses`.`id` WHERE `blocknumber`=?;", [i]);
-    let cache_block_tmp = {};
-    cnt_block+=1;
-    if (query.length !== 0) {
-      for (let j = 0; j < query.length; j++) {
+    let query = await conn.query("SELECT `address_id`, `blocknumber`, `type` FROM `states` WHERE `blocknumber`>=? AND `blocknumber`<?;", [i, Math.min(i+batch_size, to+1)]);
+    let result = Array.from(Array(batch_size), () => {
+      return [];
+    });
+    for (let j = 0; j < query.length; ++j) {
+      result[query[j].blocknumber-i].push(query[j]);
+    }
+    for (let k = 0; k < batch_size; ++k) {
+      let cache_block_tmp = {};
+      for (let j in result[k]) {
         try {
-          let addr = Buffer.from(query[j].address).toString('hex');
-          if (query[j].type % 2 == 0) { //read
-            update_account(addr, i, 0);
+          //let addr = Buffer.from(query[j].address).toString('hex');
+          let addr = result[k][j].address_id;
+          if (result[k][j].type % 2 == 0) { //read
+            update_account(addr, i+k, 0);
           } else { //write
-            update_account(addr, i, 1);
+            update_account(addr, i+k, 1);
           }
           cache_block_tmp[addr] = 1;
           cnt_state++;
         } catch {
-          console.error('Error blk #%d, j #%d', query[j].blocknumber, j);
+          console.error('Error blk #%d, j #%d', result[k][j].blocknumber, j);
         }
       }
-    }
 
-    cache_block[i] = cache_block_tmp;
+      cache_block[i+k] = cache_block_tmp;
 
-    //simulate inactivation
-    if ((i + 1 - from) % epoch_inactivate_every == 0) {
-      for (let j = 0; j < epoch_inactivate_every; j++) {
-        let block_removal = i - j - epoch_inactivate_older_than;
-        if (block_removal in cache_block) {
-          for (let j in cache_block[block_removal]) {
-            if (cache_account[j] == block_removal) {
-              cache_account[j] = -block_removal;
+      //simulate inactivation
+      if ((i+k + 1 - from) % epoch_inactivate_every == 0) {
+        for (let j = 0; j < epoch_inactivate_every; ++j) {
+          let block_removal = i+k - j - epoch_inactivate_older_than;
+          if ((''+block_removal) in cache_block) {
+            for (let j in cache_block[block_removal]) {
+              if (cache_account[j] == ''+block_removal) {
+                cache_account[j] = -block_removal;
+              }
             }
+            delete cache_block.block_removal;
           }
-          delete cache_block.block_removal;
         }
       }
+      if ((i+k) % log_period === 0) {
+        let ms = new Date() - start;
+        console.error('Blk height: %d, Blkn: %d(%d/s), Staten: %d(%d/s), Time: %dms', i+k, cnt_block, (cnt_block/ms*1000).toFixed(2), cnt_state, (cnt_state/ms*1000).toFixed(2), ms);
+      }
     }
-    if (i % log_period === 0) {
-      let ms = new Date() - start;
-      console.error('Blk height: %d, Blkn: %d(%d/s), Txn: %d(%d/s), Time: %dms', i, cnt_block, (cnt_block/ms*1000).toFixed(2), cnt_state, (cnt_state/ms*1000).toFixed(2), ms);
+    cnt_block = Math.min(cnt_block+batch_size, to);
+  }
+
+  cache_account = null;
+
+  console.error('Processing restore list');
+
+  for (let i in restore) {
+    for (let j in restore[i]) {
+      let query = await conn.query("SELECT `address` FROM `addresses` WHERE `id`=?;", [restore[i][j]]);
+      if (query.length > 0) restore[i][j] = '0x' + Buffer.from(query[0].address).toString('hex');
+      else console.log(restore[i][j]);
     }
   }
+
   conn.end();
 
   let restore_json = JSON.stringify(restore, null, "  ");
@@ -118,23 +140,23 @@ async function run(from, to) {
 }
 
 async function update_account (address, blocknumber, type) {
-  if (type == 0) { //read
+  if (type === 0) { //read
     if (address in cache_account && cache_account[address] < 0) {
       //console.log('Restore: 0x' + address + ' at #' + blocknumber + ' (last tx at #' + (-cache_account[address]) + ')');
       if (blocknumber in restore) {
-        restore[blocknumber].push('0x'+address);
+        restore[blocknumber].push(address);
       } else {
-        restore[blocknumber] = ['0x'+address];
+        restore[blocknumber] = [address];
       }
       cache_account[address] = blocknumber;
     }
-  } else if (type == 1) { //write
+  } else if (type === 1) { //write
     if (address in cache_account && cache_account[address] < 0) {
       //console.log('Restore: 0x' + address + ' at #' + blocknumber + ' (last tx at #' + (-cache_account[address]) + ')');
       if (blocknumber in restore) {
-        restore[blocknumber].push('0x'+address);
+        restore[blocknumber].push(address);
       } else {
-        restore[blocknumber] = ['0x'+address];
+        restore[blocknumber] = [address];
       }
     }
     cache_account[address] = blocknumber;
