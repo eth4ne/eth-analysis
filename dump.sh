f06ffcca8717310cd4515494cc362d88422ca635
Jiyong Youn <01@0101010101.com>
2023-01-26 00:30:23 +0900

    support dump of transactions_accesslist table

--- dump.sh
+++ dump.sh
@@ -15,6 +15,10 @@ do
   mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "number >= $start AND number <= $end" blocks > $DESTINATION/blocks_$start-$end.sql
   mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "blocknumber >= $start AND blocknumber <= $end" transactions > $DESTINATION/transactions_$start-$end.sql
   mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "blocknumber >= $start AND blocknumber <= $end" uncles > $DESTINATION/uncles_$start-$end.sql
+  if [ $i -ge 12 ]
+  then
+    mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "id >= (SELECT transactions_accesslist.id FROM transactions_accesslist LEFT JOIN transactions ON transactions_accesslist.hash=transactions.hash WHERE blocknumber>=$start AND blocknumber<=$end ORDER BY transactions_accesslist.id ASC LIMIT 1) AND id <= (SELECT transactions_accesslist.id FROM transactions_accesslist LEFT JOIN transactions ON transactions_accesslist.hash=transactions.hash WHERE blocknumber>=$start AND blocknumber<=$end ORDER BY transactions_accesslist.id DESC LIMIT 1)" transactions_accesslist > /shared/hletrd/transactions_accesslist_$start-$end.sql
+  fi
   if [ $i -eq 0 ]
   then
     range_start=$((100000))
