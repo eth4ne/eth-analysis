73901eaaadec6895acf32aa51d45a2023c4d1711
Jiyong Youn <01@0101010101.com>
2023-03-23 15:26:34 +0900

    changed contracts table structure

--- dump.sh
+++ dump.sh
@@ -16,15 +16,10 @@ do
   mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "blocknumber >= $start AND blocknumber <= $end" transactions > $DESTINATION/transactions_$start-$end.sql
   mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "blocknumber >= $start AND blocknumber <= $end" uncles > $DESTINATION/uncles_$start-$end.sql
   if [ $i -ge 12 ]
+  mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "blocknumber >= $start AND blocknumber <= $end" contracts > $DESTINATION/contracts_$start-$end.sql
   then
     mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "id >= (SELECT transactions_accesslist.id FROM transactions_accesslist LEFT JOIN transactions ON transactions_accesslist.hash=transactions.hash WHERE blocknumber>=$start AND blocknumber<=$end ORDER BY transactions_accesslist.id ASC LIMIT 1) AND id <= (SELECT transactions_accesslist.id FROM transactions_accesslist LEFT JOIN transactions ON transactions_accesslist.hash=transactions.hash WHERE blocknumber>=$start AND blocknumber<=$end ORDER BY transactions_accesslist.id DESC LIMIT 1)" transactions_accesslist > $DESTINATION/transactions_accesslist_$start-$end.sql
   fi
-  if [ $i -eq 0 ]
   then
-    range_start=$((100000))
-  else
-    range_start=$(($start+200))
   fi
-  range_end=$(($end-200))
-  mysqldump -u ethereum ethereum --lock-tables=false --skip-add-locks --skip-comments --skip-add-drop-table --no-create-info --where "id >= (SELECT id FROM contracts WHERE creationtx=(SELECT hash FROM transactions WHERE blocknumber>=$start AND blocknumber<$range_start AND \`to\` IS NULL ORDER BY id ASC LIMIT 1)) AND id <= (SELECT id FROM contracts WHERE creationtx=(SELECT hash FROM transactions WHERE blocknumber<=$end AND blocknumber>$range_end AND \`to\` IS NULL ORDER BY id DESC LIMIT 1))" contracts > $DESTINATION/contracts_$start-$end.sql
 done
